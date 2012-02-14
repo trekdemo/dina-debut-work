858242e924606ede5e86390704a60a0893e84ea0
Gergo Sulymosi <gergo.sulymosi@gmail.com>
2012-02-14 20:28:11 +0100

    Fresh rails without AR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numerals::Application.config.secret_token = '43a7d7e52f2acecc89609def0b281eb64914085b3f3407b5e07f54adf050ec676e29047585e2eaa996a7a296c5b914e784fdcace80a93a4ea7cd867c7f87eda5'
